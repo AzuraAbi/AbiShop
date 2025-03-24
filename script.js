5336d808c6a1031d8a6d32145452a9b65998f9e1
AzuraAbi <herogia41@gmail.com>
2025-03-24 19:19:45 +0700

    ok

---
+++ script.js
@@ -0,0 +1,61 @@
+const sale_next_button = document.getElementById('sale-next')
+const sale_prev_button = document.getElementById('sale-prev')
+const sale_slides = document.getElementById('sale-slides')
+
+var current_sale_index = 0
+const min_sale_index = 0
+const max_sale_index = 2
+const auto_change_sale = 5000
+var debounce = false
+
+function update_sales() {
+    const push_slide = sale_slides.querySelector('.first')
+
+    if(push_slide) {
+        document.documentElement.style.setProperty('--sale-index', current_sale_index)
+    }
+
+    debounce = true
+
+    setTimeout(() => {
+        debounce = false
+    }, 800)
+}
+
+sale_next_button.addEventListener('click', () => {
+    if(debounce) return
+
+    if(current_sale_index == max_sale_index) {
+        current_sale_index = min_sale_index
+    } else {
+        current_sale_index += 1
+    }
+
+    update_sales()
+})
+
+sale_prev_button.addEventListener('click', () => {
+    if(debounce) return
+    
+    if(current_sale_index == min_sale_index) {
+        current_sale_index = max_sale_index
+    } else {
+        current_sale_index -= 1
+    }
+
+    update_sales()
+})
+
+
+setInterval(() => {
+    if(debounce == true) return
+
+    if(current_sale_index == max_sale_index) {
+        current_sale_index = min_sale_index
+    } else {
+        current_sale_index += 1
+    }
+
+    update_sales()
+
+}, auto_change_sale);
